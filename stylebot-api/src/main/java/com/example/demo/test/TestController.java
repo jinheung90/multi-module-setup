e3522f78cdee4d9f3258429261703e5b2044fa15
jinheueng <jinheung90@gmail.com>
2022-12-10 20:36:16 +0900

    test update

--- stylebot-api/src/main/java/com/example/demo/test/TestController.java
+++ stylebot-api/src/main/java/com/example/demo/test/TestController.java
@@ -1,22 +1,21 @@
-package com.example.demo.dept;
+package com.example.demo.test;
 
 
+import com.example.demo.dept.Dept;
+import com.example.demo.dept.DeptService;
 import com.example.demo.shop.domain.Shop;
 import com.example.demo.shop.service.ShopService;
 import com.example.demo.user.domain.User;
 import com.example.demo.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Random;
 
 @RestController
 @RequiredArgsConstructor
-public class DeptController {
+public class TestController {
 
     private final DeptService deptService;
     private final UserService userService;
@@ -34,13 +33,6 @@ public class DeptController {
     @GetMapping(value = "/shop")
     public ResponseEntity<List<Shop>> findAllShop() {
         return ResponseEntity.ok(shopService.findAll());
-    }
-
-
-    @PostMapping(value = "/dept")
-    public ResponseEntity<String> save(@RequestBody DeptDTO deptDTO) {
-        deptService.insert(deptDTO);
-        return ResponseEntity.ok("success");
     }
 
 }
