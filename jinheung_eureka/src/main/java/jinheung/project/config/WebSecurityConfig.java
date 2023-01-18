39303fe1dd428778265f53f271d8a6624fba8c57
dawin <jinheung90@gmail.com>
2023-01-18 17:06:59 +0900

    auth seperated and ecr

--- jinheung_eureka/src/main/java/jinheung/project/config/WebSecurityConfig.java
+++ jinheung_eureka/src/main/java/jinheung/project/config/WebSecurityConfig.java
@@ -2,10 +2,14 @@ package jinheung.project.config;
 
 
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -13,36 +17,10 @@ import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-@Configuration
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-//    @Override
-//    public void configure(AuthenticationManagerBuilder auth) throws Exception {
-//
-//        auth.inMemoryAuthentication()
-//            .passwordEncoder(org.springframework.security.crypto.password.NoOpPasswordEncoder.getInstance())
-//            .withUser(name).password(password)
-//            .authorities("ADMIN");
-//    }
+@RequiredArgsConstructor
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig  {
 
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-            .csrf()
-            .disable()
-            .authorizeRequests()
-            .antMatchers("/**").permitAll()
-            .mvcMatchers("/**").permitAll()
-//
-//            .formLogin()
-//            .failureForwardUrl("/")
-            ;
-//
-//            .authorizeRequests()
-//            .anyRequest().authenticated()
-//
-//            .and()
-//            .httpBasic();
-    }
 }
