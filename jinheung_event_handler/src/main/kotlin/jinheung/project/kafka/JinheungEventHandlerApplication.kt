149a9bd77dc5bcfa74e455824dd18488024b25d3
dawin <jinheung90@gmail.com>
2023-01-18 15:43:59 +0900

    auth seperated and ecr

---
+++ jinheung_event_handler/src/main/kotlin/jinheung/project/kafka/JinheungEventHandlerApplication.kt
@@ -0,0 +1,12 @@
+package jinheung.project.kafka
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+
+
+@SpringBootApplication
+class JinheungEventHandlerApplication
+fun main(args: Array<String>) {
+	runApplication<JinheungEventHandlerApplication>(*args)
+}
