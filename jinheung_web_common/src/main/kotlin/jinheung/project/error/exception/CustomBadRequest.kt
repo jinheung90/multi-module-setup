7e588cb00da451e7312a6b6674c885e330fdb036
jinheueng <jinheung90@gmail.com>
2023-03-12 14:03:58 +0900

    tf

--- jinheung_web_common/src/main/kotlin/jinheung/project/error/exception/CustomBadRequest.kt
+++ jinheung_web_common/src/main/kotlin/jinheung/project/error/exception/CustomBadRequest.kt
@@ -2,20 +2,9 @@ package jinheung.project.error.exception
 
 import jinheung.project.error.enums.GlobalErrorCode
 
-class CustomBadRequest : RuntimeException {
+class CustomBadRequest(private var code: GlobalErrorCode, customMessage: String?) : RuntimeException(customMessage) {
 
-    private var code : GlobalErrorCode = GlobalErrorCode.BAD_REQUEST
     override val message : String = ""
-    constructor(message: String, ex: Exception?): super(message, ex) {}
-    constructor(message: String): super(message) {}
-    constructor(ex: Exception): super(ex) {}
-    constructor(code: GlobalErrorCode) : super(code.getMessage()) {
-        this.code = code
-    }
-
-    constructor(code: GlobalErrorCode, customMessage: String?) : super(customMessage) {
-        this.code = code
-    }
 
     fun getCode() : GlobalErrorCode {
         return this.code
