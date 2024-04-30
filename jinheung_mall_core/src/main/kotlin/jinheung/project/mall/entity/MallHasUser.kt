539273f7e08be9438f3ee66e54ff52cf5545c83d
jinheueng <jinheung90@gmail.com>
2024-04-30 16:22:11 +0900

    delete data keyword

--- jinheung_mall_core/src/main/kotlin/jinheung/project/mall/entity/MallHasUser.kt
+++ jinheung_mall_core/src/main/kotlin/jinheung/project/mall/entity/MallHasUser.kt
@@ -3,12 +3,7 @@ package jinheung.project.mall.entity
 import jakarta.persistence.*
 
 
-@Entity(name = "mals_has_users")
-//    uniqueConstraints = [
-//        UniqueConstraint(
-//            name = "mall_user_unique",
-//            columnNames = [ "mall_id",  "user_id" ])
-//    ])
+@Entity(name = "malls_has_users")
 class MallHasUser(
     @Column(name = "user_id", nullable = false)
     val userId: Long,
@@ -16,7 +11,6 @@ class MallHasUser(
     @JoinColumn(name = "mall_id")
     val mall: Mall = Mall()
 ) {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long = 0
